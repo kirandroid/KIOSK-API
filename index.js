ad79e23e05f6cdf5d9b0bb640828b52787ac29e9
num3 <pradhanangakiran@gmail.com>
2019-05-09 10:16:38 +0545

    added File Upload request

--- index.js
+++ index.js
@@ -3,14 +3,37 @@ const express = require('express');
 const app = express();
 const oracledb = require('oracledb');
 var compression = require('compression');
+const fileUpload = require('express-fileupload');
 var helmet = require('helmet');
 app.use(helmet());
 app.use(express.json());
 app.use(compression());
+const path = require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 const port = process.env.PORT || 4000;
+
+//images stored in uploads folder is served through - http://localhost:4000/images/<imagename>
+app.use('/images', express.static(__dirname + '/uploads/'));
+
+app.use(fileUpload());
+
+app.post('/api/upload', function(req, res) {
+	if (Object.keys(req.files).length == 0) {
+		return res.status(400).send('No files were uploaded.');
+	}
+	let imageFile = req.files.imageFile;
+
+	imageFile.mv(__dirname + '/uploads/' + imageFile.name, function(err, result) {
+		if (err) return res.status(500).send(err);
+
+		res.send({
+			status: 'Success yo!',
+			url: 'http://kioskapi.tk:4000/images/' + imageFile.name
+		});
+	});
+});
 
 const config = {
 	user: 'root',
@@ -24,6 +47,19 @@ app.get('/api/users', (req, res) => {
 	oracledb.getConnection(config, function(err, connection) {
 		if (err) throw err;
 		connection.execute('select * from USERS_TT', {}, { outFormat: oracledb.OBJECT }, function(err, rows) {
+			if (err) throw err;
+			res.send(rows.rows);
+		});
+	});
+});
+
+app.get('/api/featured_home_slider', (req, res) => {
+	oracledb.getConnection(config, function(err, connection) {
+		if (err) throw err;
+		connection.execute('select * from FEATURED_HOME_SLIDER', {}, { outFormat: oracledb.OBJECT }, function(
+			err,
+			rows
+		) {
 			if (err) throw err;
 			res.send(rows.rows);
 		});
@@ -48,16 +84,11 @@ app.post(
 				[ req.body.USERNAME, req.body.PASSWORD ],
 				{ outFormat: oracledb.OBJECT },
 				function(err, rows) {
-					// if (err) {
-					// 	res.send(err);
-					// }
-					console.log(err);
-					res.send(rows.rows);
-					// if (rows.length > 0) {
-					// 	res.send(rows.rows);
-					// } else {
-					// 	res.send('Incorrect Username and Password!');
-					// }
+					if (rows.length > 0) {
+						res.send(rows.rows);
+					} else {
+						res.send(err);
+					}
 				}
 			);
 		});
